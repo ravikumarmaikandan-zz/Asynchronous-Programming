5234c204d7ac72c5f0a3a46fabd96256bf7261f0
Ravikumar Maikandan <ravi.maikandan@gmail.com>
2016-07-05 16:28:22 -0500

    Async Programming

--- AsynchronousProgramming/AsynchronousProgramming/MainWindow.xaml.cs
+++ AsynchronousProgramming/AsynchronousProgramming/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,86 @@ namespace AsynchronousProgramming
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void BtnLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            //btnLogin.IsEnabled = false;
+            // var task = Task.Run(() =>
+            // {
+            //    Thread.Sleep(2000);
+            //     return "Login Successfull";
+            // });
+
+            //task.ContinueWith((t) =>
+            //{
+            //    Dispatcher.Invoke(() =>
+            //    {
+            //        btnLogin.IsEnabled = true;
+            //        btnLogin.Content = t.Result;
+            //    });
+            //});
+            //task.ConfigureAwait(true).GetAwaiter().OnCompleted(() =>
+            //{
+            //    if (task.IsFaulted)
+            //    {
+            //        btnLogin.IsEnabled = true;
+            //        btnLogin.Content = "Login Failed!!";
+
+            //    }
+            //    else
+            //    {
+            //        btnLogin.IsEnabled = true;
+            //        btnLogin.Content = task.Result;
+
+            //    }
+            //});
+            try
+            {
+                var result = await LoginAsync();
+                btnLogin.Content = result;
+
+            }
+            catch (Exception)
+            {
+                
+               ;
+                btnLogin.Content = "Internal Error!!!";
+            }
+            
+
+        }
+
+        public async Task<string> LoginAsync()
+        {
+            try
+            {
+                //Firing single task with return result
+                //var result = await Task.Run(() =>
+                //{
+                //    Thread.Sleep(2000);
+                //    return "Login Successful!!";
+                //});
+
+                //Firing multiple task at the same time (parallel execution)
+                var firstTask = Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    return "Login Successful!!";
+                });
+
+                var secondTask = Task.Delay(2000);
+                var thirstTask = Task.Delay(1000);
+
+                await Task.WhenAll(firstTask, secondTask, thirstTask);
+                return firstTask.Result;
+            }
+            catch (Exception)
+            {
+
+                return "Login Failed!!";
+            }
+
         }
     }
 }
